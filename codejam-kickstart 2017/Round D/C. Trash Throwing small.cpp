326c539531c1c799610db8b683ca71b14ea887e5
Shuangquan Feng <fengsq@utexas.edu>
2018-07-25 20:15:53 -0500

    round D C small

--- codejam-kickstart 2017/Round D/C. Trash Throwing small.cpp
+++ codejam-kickstart 2017/Round D/C. Trash Throwing small.cpp
@@ -25,28 +25,30 @@ inline double dis(double x1, double y1, double x2, double y2) {
 }
 
 bool safe(double a, double x, double y, double rad) {
-	// cout << a << ' ' << x << ' ' << y << ' '<< rad << endl;
 	if (y>=rad) return true;
 	double l = 0, r = p/2;
+	double last_dis = min(dis(x,y,0,0), dis(x,y,r,a*r*(r-p)));
 	while (r-l>=1e-5) {
 		double d = r-l, u=l+d/3, v=l+2*d/3;
 		double d1 = dis(x, y, u, a*u*(u-p));
 		double d2 = dis(x, y, v, a*v*(v-p));
-		// printf("%lf %lf %lf %lf %lf %lf\n", l, r, u, d1, v, d2);
+		double now_dis = min(d1, d2);
+		if (fabs(now_dis-last_dis)<=1e-5) {
+			l = u;
+			break;
+		}
 		if (d1<d2)
 			r = v;
 		else if (d1>d2) 
 			l = u;
 		else 
 			l = u, r = v; 
-		// printf("%lf %lf %lf %lf %lf %lf %lf\n", l, r, u, d1, v, d2, r-l);
 	}
-	// printf("%lf %lf %lf %lf %lf\n", a, -pow(p,2)*a/4, l, a*l*(l-p), dis(x, y, l, a*l*(l-p)));
 	return dis(x, y, l, a*l*(l-p)) >= rad*rad;
 }
 
 bool ok(double r) {
-	for (double t=0; t<=h-r; t+=0.0005) {
+	for (double t=0; t<=h-r; t+=h-r) {
 		double a = -4*t/pow(p, 2);
 		bool valid = true;
 		REP(i, 1, n)
@@ -60,7 +62,6 @@ bool ok(double r) {
 }
 
 int main() {
-	// ios::sync_with_stdio(false);
 	int T;
 	cin >> T;
 
@@ -72,13 +73,11 @@ int main() {
 
 		while (r-l>=1e-5) {
 			double rad = (l+r) / 2;
-			// printf("%lf %lf %lf\n", l, r, rad);
 			if (ok(rad))
 				l = rad;
 			else
 				r = rad;
 		}
-		// cout << std::setprecision(6) << l << '\n';
 		printf("%.6lf\n", l);
 	}
 
